f893330bf3ef63bfa1af97da36a57d3509b1bdce
Josehpequeno <hicarojbs21@gmail.com>
2021-08-31 16:13:16 -0300

    liberar_processo_exato implementada com falha

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+#![allow(unused)]
+#![feature(linked_list_remove)]// `#![feature]` may not be used on the stable release channel
+//rustup install nightly
+//cargo +nightly install racer
+//cargo +nightly test
+//#![feature(repr128)]
+//#[repr(u128)]            
 use std::collections::LinkedList;
 #[derive(Clone)]
 struct Bloco {
@@ -15,65 +22,49 @@ fn build_bloco (nome: String) -> Bloco {
     b    
 }
 
-// struct Lista {
-//     tamanho: i32,
-//     primeiro: Option<Box<Bloco>>,
-//     ultimo: Option<Box<Bloco>>,
-// }
-// fn build_lista () -> Lista {
-//     let l = Lista {
-//         tamanho: 0,
-//         primeiro: None,
-//         ultimo: None,
-//     };
-//     l
-// }
-// fn adiciona_bloco (nome: String, lista: &mut Lista) {
-//     let mut b = build_bloco(nome);
-//     if lista.tamanho == 0 {
-//         b.indice = 0;
-//         lista.primeiro = Some(Box::new(b));
-//         lista.ultimo = lista.primeiro;
-//         lista.tamanho +=1;
-//     } else {
-//         b.indice = lista.tamanho;
-//         match lista.ultimo { 
-//             Some(x) => match x.proximo {
-//                 Some(mut y) => {
-//                     y = Box::new(b);
-//                     lista.ultimo = Some(y);
-//                 },
-//                 None => print!("Err"),
-//             },
-//             None => print!("Err"),
-//         }
-//         lista.tamanho +=1;
-//     }  
-// }
-// fn print_lista (lista: Box<Lista>) {
-//     if lista.tamanho == 0 {
-//         println!()
-//     }
-// }
-
 fn alocar_novo_processo(nome: String, lista: &mut LinkedList<Bloco>) {
-    let mut b = build_bloco(nome);
+    let b = build_bloco(nome);
     lista.push_back(b);
 }
 
-fn liberar_processo(listaAlocada: &mut LinkedList<Bloco>, listaLivre: &mut LinkedList<Bloco>){
-    let mut l = *listaAlocada.back().clone().unwrap();
-    listaLivre.push_back(l);
-    listaAlocada.pop_back();
+fn liberar_processo(lista_liberando: &mut LinkedList<Bloco>, lista_recebendo: &mut LinkedList<Bloco>){
+    //let nome = String::from(&lista_alocada.front().clone().unwrap().nome_processo);
+    //lista_livre.push_back(build_bloco(nome));
+    let b = lista_liberando.front_mut().cloned().unwrap();
+    lista_recebendo.push_back(b);
+    lista_liberando.pop_front();
+}
+fn liberar_processo_exato(lista_liberando: &mut LinkedList<Bloco>, lista_recebendo: &mut LinkedList<Bloco>, processo: String){
+    let mut flag = false;
+    let ll = lista_liberando;
+    let mut int = 0;
+    for (i, bloco) in ll.iter().enumerate() {
+        if bloco.nome_processo == processo {
+            lista_recebendo.push_back(*bloco);
+            int = i;
+            flag = true;
+        }
+    }
+    if !flag {
+        println!("Processo inexistente!");
+    }else { 
+        ll.remove(int);
+    }
 }
 
-fn print_lista (lista: LinkedList<Bloco>) {
+
+fn print_lista (lista: &mut LinkedList<Bloco>) {
+    print!("Inicio => ");
+    if lista.is_empty() {
+        println!("Lista vazia!");
+        return
+    }
     for (i,bloco) in lista.iter().enumerate() {
         if i != lista.len()-1 {
-            print!("{} -> ", bloco.nome_processo);
+            print!("|{}| -> ", bloco.nome_processo);
         }
         else {
-            println!("{}", bloco.nome_processo);
+            println!("|{}|", bloco.nome_processo);
         }
     }
 }
@@ -83,11 +74,23 @@ fn main() {
     // print_lista(lista);
     let mut paginas_alocadas: LinkedList<Bloco> = LinkedList::new();
     let mut paginas_livres: LinkedList<Bloco> = LinkedList::new();
-    alocar_novo_processo(String::from("ei"), &mut paginas_alocadas);
-    alocar_novo_processo(String::from("fala"), &mut paginas_alocadas);
-    alocar_novo_processo(String::from("nada não"), &mut paginas_alocadas);
+    let pa = &mut paginas_alocadas;
+    let pl = &mut paginas_livres;
+    alocar_novo_processo(String::from("ei"), pa);
+    alocar_novo_processo(String::from("fala"), pa);
+    alocar_novo_processo(String::from("nada não"), pa);
     println!("Páginas Alocadas: ");
-    print_lista(paginas_alocadas);
+    print_lista(pa);
     println!("Páginas Alocadas: ");
-    print_lista(paginas_livres)
+    print_lista(pl);
+    liberar_processo(pa, pl);
+    println!("Páginas Alocadas: ");
+    print_lista(pa);
+    println!("Páginas Alocadas: ");
+    print_lista(pl);
+    liberar_processo(pl, pa);
+    println!("Páginas Alocadas: ");
+    print_lista(pa);
+    println!("Páginas Alocadas: ");
+    print_lista(pl);
 }
