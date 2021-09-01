48468fa07c175c2b47d4cc4e1fe885484dea3701
Josehpequeno <hicarojbs21@gmail.com>
2021-09-01 01:08:46 -0300

    implementando menu

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # FIFO-buffer
+### instabilidade no remove do collections::LinkedList
+``` rust
+#![feature(linked_list_remove)]// `#![feature]` may not be used on the stable release channel
+```
+### Instalando e executando um canal instável
+```bash
+rustup install nightly
+cargo +nightly install racer
+cargo +nightly test
+cargo +nightly run
+```
+
+### Preview
+<img src="img/preview.png">
--- src/main.rs
+++ src/main.rs
@@ -1,96 +1,286 @@
 #![allow(unused)]
-#![feature(linked_list_remove)]// `#![feature]` may not be used on the stable release channel
-//rustup install nightly
-//cargo +nightly install racer
-//cargo +nightly test
-//#![feature(repr128)]
-//#[repr(u128)]            
+#![feature(linked_list_remove)] // `#![feature]` may not be used on the stable release channel
+                                //rustup install nightly
+                                //cargo +nightly install racer
+                                //cargo +nightly test
+                                //cargo +nightly run
+                                //#![feature(repr128)]
+                                //#[repr(u128)]
+use nanoid::nanoid;
 use std::collections::LinkedList;
+
+extern crate rustbox;
+
+use std::default::Default;
+use std::error::Error;
+
+use rustbox::Key;
+use rustbox::{Color, RustBox};
+
+extern crate term_size;
+
 #[derive(Clone)]
-struct Bloco {
-    nome_processo: String,
+struct Pagina {
+    nome_pagina: String,
+    id: String,
     //indice: i32,
-    //proximo: Option<Box<Bloco>>,//  Box -> senão o Bloco vai ao infinito.
+    //proximo: Option<Box<Pagina>>,//  Box -> senão o Pagina vai ao infinito.
 }
 
-fn build_bloco (nome: String) -> Bloco {
-    let b = Bloco {
-        nome_processo: nome,
+fn build_pagina(nome: String, l1: &mut LinkedList<Pagina>, l2: &mut LinkedList<Pagina>) -> Pagina {
+    let mut i = 4;
+    let alphabet: [char; 10] = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
+    let mut id = nanoid!(i, &alphabet);
+    let mut id_ref = &id;
+    while busca_id(id_ref, l1, l2) {
+        i += 1;
+        id = nanoid!(i, &alphabet);
+        id_ref = &id;
+    }
+    let b = Pagina {
+        nome_pagina: nome,
+        id: id,
         //indice: 0,
         //proximo: None,
     };
-    b    
+    b
 }
 
-fn alocar_novo_processo(nome: String, lista: &mut LinkedList<Bloco>) {
-    let b = build_bloco(nome);
+fn busca_id(id: &String, l1: &mut LinkedList<Pagina>, l2: &mut LinkedList<Pagina>) -> bool {
+    let mut flag = false;
+    for pagina in l1.iter() {
+        if pagina.id == *id {
+            flag = true;
+        }
+    }
+    for pagina in l2.iter() {
+        if pagina.id == *id {
+            flag = true;
+        }
+    }
+    flag
+}
+
+fn alocar_novo_pagina(nome: String, lista: &mut LinkedList<Pagina>, l2: &mut LinkedList<Pagina>) {
+    let b = build_pagina(nome, lista, l2);
     lista.push_back(b);
 }
 
-fn liberar_processo(lista_liberando: &mut LinkedList<Bloco>, lista_recebendo: &mut LinkedList<Bloco>){
-    //let nome = String::from(&lista_alocada.front().clone().unwrap().nome_processo);
-    //lista_livre.push_back(build_bloco(nome));
+fn liberar_pagina(
+    lista_liberando: &mut LinkedList<Pagina>,
+    lista_recebendo: &mut LinkedList<Pagina>,
+) {
+    //let nome = String::from(&lista_alocada.front().clone().unwrap().nome_pagina);
+    //lista_livre.push_back(build_Pagina(nome));
+    if lista_liberando.is_empty() {
+        println!("Requisitando liberação de página em uma Lista vazia!");
+        return;
+    }
     let b = lista_liberando.front_mut().cloned().unwrap();
     lista_recebendo.push_back(b);
     lista_liberando.pop_front();
 }
-fn liberar_processo_exato(lista_liberando: &mut LinkedList<Bloco>, lista_recebendo: &mut LinkedList<Bloco>, processo: String){
+fn alocar_pagina_exata(
+    lista_liberando: &mut LinkedList<Pagina>,
+    lista_recebendo: &mut LinkedList<Pagina>,
+    pagina_nome: String,
+) {
+    if lista_liberando.is_empty() {
+        println!("Requisitando liberação de página em uma Lista vazia!");
+        return;
+    }
     let mut flag = false;
     let ll = lista_liberando;
     let mut int = 0;
-    for (i, bloco) in ll.iter().enumerate() {
-        if bloco.nome_processo == processo {
-            lista_recebendo.push_back(*bloco);
+    for (i, pagina) in ll.iter().enumerate() {
+        if pagina.nome_pagina == pagina_nome {
+            let b = pagina.to_owned();
+            lista_recebendo.push_back(b);
             int = i;
             flag = true;
         }
     }
     if !flag {
-        println!("Processo inexistente!");
-    }else { 
+        println!("pagina inexistente!");
+    } else {
         ll.remove(int);
     }
 }
 
-
-fn print_lista (lista: &mut LinkedList<Bloco>) {
-    print!("Inicio => ");
+fn print_lista(lista: &mut LinkedList<Pagina>) -> &str {
+    let mut s = String::new();
+    s.push_str(" Inicio => ");
     if lista.is_empty() {
-        println!("Lista vazia!");
-        return
+        s.push_str("Lista vazia!");
+        let s_slice: &str = Box::leak(s.into_boxed_str());
+        return s_slice; // convertendo String em &str
     }
-    for (i,bloco) in lista.iter().enumerate() {
-        if i != lista.len()-1 {
-            print!("|{}| -> ", bloco.nome_processo);
-        }
-        else {
-            println!("|{}|", bloco.nome_processo);
+    for (i, pagina) in lista.iter().enumerate() {
+        if i != lista.len() - 1 {
+            s.push_str("[");
+            s.push_str(&pagina.nome_pagina[..]);
+            s.push_str(" | id: ");
+            s.push_str(&pagina.id[..]);
+            s.push_str("] -> ");
+        } else {
+            s.push_str("[");
+            s.push_str(&pagina.nome_pagina[..]);
+            s.push_str(" | id: ");
+            s.push_str(&pagina.id[..]);
+            s.push_str("] -> ");
         }
     }
+    Box::leak(s.into_boxed_str())
 }
 fn main() {
-    // let mut lista = Box::new(build_lista());
-    // adiciona_bloco(String::from("ei"), &mut lista);
-    // print_lista(lista);
-    let mut paginas_alocadas: LinkedList<Bloco> = LinkedList::new();
-    let mut paginas_livres: LinkedList<Bloco> = LinkedList::new();
+    let mut paginas_alocadas: LinkedList<Pagina> = LinkedList::new();
+    let mut paginas_livres: LinkedList<Pagina> = LinkedList::new();
     let pa = &mut paginas_alocadas;
     let pl = &mut paginas_livres;
-    alocar_novo_processo(String::from("ei"), pa);
-    alocar_novo_processo(String::from("fala"), pa);
-    alocar_novo_processo(String::from("nada não"), pa);
-    println!("Páginas Alocadas: ");
-    print_lista(pa);
-    println!("Páginas Alocadas: ");
-    print_lista(pl);
-    liberar_processo(pa, pl);
-    println!("Páginas Alocadas: ");
-    print_lista(pa);
-    println!("Páginas Alocadas: ");
-    print_lista(pl);
-    liberar_processo(pl, pa);
-    println!("Páginas Alocadas: ");
-    print_lista(pa);
-    println!("Páginas Alocadas: ");
-    print_lista(pl);
+    // //alocando
+    // alocar_novo_pagina("ei".to_string(), pa, pl);
+    // alocar_novo_pagina("fala".to_string(), pa, pl);
+    // alocar_novo_pagina("nada não".to_string(), pa, pl);
+    // println!("Páginas Alocadas: ");
+    // print_lista(pa);
+    // println!("Páginas Livres: ");
+    // print_lista(pl);
+    // //liberando
+    // liberar_pagina(pa, pl);
+    // println!("Páginas Alocadas: ");
+    // print_lista(pa);
+    // println!("Páginas Livres: ");
+    // print_lista(pl);
+    // liberar_pagina(pl, pa);
+    // println!("Páginas Alocadas: ");
+    // print_lista(pa);
+    // println!("Páginas Livres: ");
+    // print_lista(pl);
+    // liberar_pagina(pa, pl);
+    // println!("Páginas Alocadas: ");
+    // print_lista(pa);
+    // println!("Páginas Livres: ");
+    // print_lista(pl);
+    // liberar_pagina(pa, pl);
+    // println!("Páginas Alocadas: ");
+    // print_lista(pa);
+    // println!("Páginas Livres: ");
+    // print_lista(pl);
+
+    // // liberando pagina exata
+    // alocar_pagina_exata(pl, pa, "nada não".to_string());
+    // println!("Páginas Alocadas: ");
+    // print_lista(pa);
+    // println!("Páginas Livres: ");
+    // print_lista(pl);
+
+    //Menu
+    let rustbox = match RustBox::init(Default::default()) {
+        Result::Ok(v) => v,
+        Result::Err(e) => panic!("{}", e),
+    };
+    let bem_vindo = " Bem vindo ao Simulador de FIFO-buffer!! ";
+    if let Some((width, height)) = term_size::dimensions() {
+        rustbox.print(
+            (width - bem_vindo.len()) / 2,
+            1,
+            rustbox::RB_BOLD,
+            Color::White,
+            Color::Red,
+            bem_vindo,
+        );
+        rustbox.print(
+            1,
+            3,
+            rustbox::RB_BOLD,
+            Color::White,
+            Color::Default,
+            "Escolha uma das Opções abaixo:  ",
+        );
+        rustbox.print(
+            1,
+            4,
+            rustbox::RB_BOLD,
+            Color::White,
+            Color::Default,
+            " Pressione '1' para alocar uma nova página.",
+        );
+        rustbox.print(
+            1,
+            5,
+            rustbox::RB_BOLD,
+            Color::White,
+            Color::Default,
+            " Pressione '2' para liberar uma página das páginas alocadas. ",
+        );
+        rustbox.print(
+            1,
+            6,
+            rustbox::RB_BOLD,
+            Color::White,
+            Color::Default,
+            " Pressione '3' para alocar uma página das páginas livre. ",
+        );
+        rustbox.print(
+            1,
+            6,
+            rustbox::RB_BOLD,
+            Color::White,
+            Color::Default,
+            " Pressione '4' para alocar uma página exata das páginas livres. ",
+        );
+        rustbox.print(
+            1,
+            7,
+            rustbox::RB_BOLD,
+            Color::White,
+            Color::Default,
+            " Pressione 'q' para sair. ",
+        );
+        rustbox.print(
+            1,
+            9,
+            rustbox::RB_BOLD,
+            Color::White,
+            Color::Default,
+            "Páginas Alocadas: ",
+        );
+        rustbox.print(
+            1,
+            10,
+            rustbox::RB_BOLD,
+            Color::White,
+            Color::Default,
+            print_lista(pa),
+        );
+        rustbox.print(
+            1,
+            12,
+            rustbox::RB_BOLD,
+            Color::White,
+            Color::Default,
+            "Páginas Livres:  ",
+        );
+        rustbox.print(
+            1,
+            13,
+            rustbox::RB_BOLD,
+            Color::White,
+            Color::Default,
+            print_lista(pl),
+        );
+    }
+    rustbox.present();
+    loop {
+        match rustbox.poll_event(false) {
+            Ok(rustbox::Event::KeyEvent(key)) => match key {
+                Key::Char('q') => {
+                    break;
+                }
+                _ => {}
+            },
+            Err(e) => panic!("{}", e),
+            _ => {}
+        }
+    }
 }
